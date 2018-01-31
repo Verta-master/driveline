851429b4347ee9823509590c19e11874cb3f8c19
Verta-master <vertyagina@gmail.com>
2018-01-31 16:44:15 +0600

    Update 2018-01-31T10:44:11.233Z

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ $('.menu__btn').click(function() {
   $('.menu').toggleClass('menu--opened');
 });
 
+$('.header__more').click(function() {
+  $('.header__address').slideToggle();
+  $('.header__email').slideToggle();
+  $(this).toggleClass('header__more--rotate');
+});
+
 // Sticky header on scroll 
 $(document).ready(function(){
   $(".header__bottom").sticky({
